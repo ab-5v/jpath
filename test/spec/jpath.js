b12ce610415e0ea175fa64860cedae86d2fe2acf
Artur Burtsev <artjock@gmail.com>
2012-07-20 17:12:23 +0400

    more tests

--- test/spec/jpath.js
+++ test/spec/jpath.js
@@ -58,6 +58,57 @@ describe('jpath', function() {
 
     });
 
+    describe('exotic', function() {
+        var json = {
+            'a': {
+                'a-b': 1,
+                '1': 2,
+                'undefined': 3,
+                '0': 4
+            }
+        };
+
+        it('.a.a-b +', function() {
+            expect(jpath(json, '.a.a-b')).toEqual([1]);
+        });
+
+        it('.a.1 +', function() {
+            expect(jpath(json, '.a.1')).toEqual([2]);
+        });
+
+        it('.a.undefined +', function() {
+            expect(jpath(json, '.a.undefined')).toEqual([3]);
+        });
+
+        it('.a.0 +', function() {
+            expect(jpath(json, '.a.0')).toEqual([4]);
+        });
+
+        it('.a[.a-b == \'1\'].1 +', function() {
+            expect(jpath(json, '.a[.a-b == \'1\'].1')).toEqual([2]);
+        });
+
+        it('.a[.a-b == \'2\'] -', function() {
+            expect(jpath(json, '.a[.a-b == "2"]')).toEqual([]);
+        });
+
+        it('.a[.1 == "2"].1 +', function() {
+            expect(jpath(json, '.a[.1 == "2"].1')).toEqual([2]);
+        });
+
+        it('.a[.0 == "4"] +', function() {
+            expect(jpath(json, '.a[.1 == "2"]')).toEqual([json.a]);
+        });
+
+        it('.a[.undefined].1 +', function() {
+            expect(jpath(json, '.a[.undefined].1')).toEqual([2]);
+        });
+
+        it('.a[.b-a].undefined +', function() {
+            expect(jpath(json, '.a[.b-a].undefined')).toEqual([]);
+        });
+    });
+
     describe('full', function() {
         var json = {
             a: 1,
