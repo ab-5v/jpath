55c646cd99c034b443048c76ab09575ca726324e
Artur Burtsev <artjock@gmail.com>
2012-07-21 00:19:36 +0400

    ie8 fix

--- jpath.js
+++ jpath.js
@@ -4,8 +4,9 @@
  * @example
  *  jpath(json, '/.foo[.bar == "3" && !.gop || .soo].lop');
  *
- * @version 0.0.3
+ * @version 0.0.4
  * @author Artur Burtsev <artjock@gmail.com>
+ * @link https://github.com/artjock/jpath
  *
  */
 
