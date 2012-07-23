b0656690234f9a29ac1b0f2e1a984fca3e5c474a
Artur Burtsev <artjock@gmail.com>
2012-07-23 19:24:51 +0400

    v0.0.7

--- jpath.js
+++ jpath.js
@@ -4,7 +4,7 @@
  * @example
  *  jpath(json, '/.foo[.bar == "3" && !.gop || .soo].lop');
  *
- * @version 0.0.6
+ * @version 0.0.7
  * @author Artur Burtsev <artjock@gmail.com>
  * @link https://github.com/artjock/jpath
  *
